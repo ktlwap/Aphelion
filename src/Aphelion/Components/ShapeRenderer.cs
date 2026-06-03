1bd243ffc1be171ce6091234d362c161cf792784
ktlwap <149824361+ktlwap@users.noreply.github.com>
2026-06-03 21:50:10 +0200

    Add sprite renderer

--- src/Aphelion/Components/ShapeRenderer.cs
+++ src/Aphelion/Components/ShapeRenderer.cs
@@ -7,21 +7,12 @@ namespace Aphelion.Components;
 
 public sealed class ShapeRenderer : BaseComponent
 {
-    public bool IsVisible { get; set; }
-    public Vector2 Size { get; set; }
-    public Vector2 Anchor { get; set; }
-    public Color Color { get; set; }
-    public float ZIndex { get; set; }
-    
-    public ShapeRenderer()
-    {
-        IsVisible = true;
-        Size = Vector2.One;
-        Anchor = Vector2.Zero;
-        Color = Color.White;
-        ZIndex = 0;
-    }
-    
+    public bool IsVisible { get; set; } = true;
+    public Vector2 Size { get; set; } = Vector2.One;
+    public Vector2 Anchor { get; set; } = Vector2.Zero;
+    public Color Color { get; set; } = Color.White;
+    public float ZIndex { get; set; } = 0;
+
     public override void Render(DrawCommandBuffer buffer)
     {
         if (!IsVisible)
